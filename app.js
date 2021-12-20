dcaed72e68a24b355d629c753e740f2c8b0d7f77
Anonno Ruddho <ahmedrimon377472@gmail.com>
2021-12-20 12:45:26 +0600

    last done callback hell

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
-// callback function start
+// // callback function start
 
-function handleUpperCase(value) {
-  console.log(value.toUpperCase());
-}
+// function handleUpperCase(value) {
+//   console.log(value.toUpperCase());
+// }
 
-function handleReverse(value) {
-  console.log(value.split("").reverse().join("").toUpperCase());
-}
+// function handleReverse(value) {
+//   console.log(value.split("").reverse().join("").toUpperCase());
+// }
 
-// callback function End
+// // callback function End
 
-function handleName(name, cb) {
-  const fullName = `${name} ahmed`;
-  cb(fullName);
-}
+// function handleName(name, cb) {
+//   const fullName = `${name} ahmed`;
+//   cb(fullName);
+// }
 
-handleName("Iftekhar", handleUpperCase);
-handleName("Iftekhar", handleReverse);
+// handleName("Iftekhar", handleUpperCase);
+// handleName("Iftekhar", handleReverse);
+
+const first = document.querySelector(".first");
+const second = document.querySelector(".second");
+const third = document.querySelector(".third");
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  setTimeout(() => {
+    first.style.color = "red";
+  }, 1000);
+  setTimeout(() => {
+    second.style.color = "blue";
+  }, 3000);
+  setTimeout(() => {
+    third.style.color = "green";
+  }, 2000);
+});
